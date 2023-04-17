3c04bde2bae05a25d0c83af7c0f88ef71b7c02e0
Marcel-SFSU <mazouri@sfsu.edu>
2023-04-17 16:44:21 -0700

    Created file

---
+++ FinalProject_03.py
@@ -0,0 +1,4 @@
+filename1="FinalProjectFiles/handwriting_test_set_labels.txt"
+filename2="FinalProjectFiles/handwriting_test_set.txt"
+filename3="FinalProjectFiles/handwriting_training_set_labels.txt"
+filename4="FinalProjectFiles/handwriting_training_set.txt"
