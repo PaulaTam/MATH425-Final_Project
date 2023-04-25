33fc5ab46e2b8aa24f46f1fe6a256eeb9dc87e06
PaulaTam <ptam4@sfsu.edu>
2023-04-24 20:03:04 -0700

    added imports, clarified filenames, started function to read set+labels

--- FinalProject_03.py
+++ FinalProject_03.py
@@ -1,4 +1,20 @@
-filename1="FinalProjectFiles/handwriting_test_set_labels.txt"
-filename2="FinalProjectFiles/handwriting_test_set.txt"
-filename3="FinalProjectFiles/handwriting_training_set_labels.txt"
-filename4="FinalProjectFiles/handwriting_training_set.txt"
+import numpy as np
+import sympy as sym
+
+training_set_labels="./FinalProjectFiles/handwriting_training_set_labels.txt"
+training_set="./FinalProjectFiles/handwriting_training_set.txt" #4000x400
+test_set_labels="./FinalProjectFiles/handwriting_test_set_labels.txt" 
+test_set="./FinalProjectFiles/handwriting_test_set.txt" #1000x400
+
+#Each example is 20pixel by 20pixel
+#Currently need to replace the value 10 in labels as 0
+
+#Make classifier for handwriting
+#Use training set and compute SVD fo each class/digit matrix
+#Use SVD to do classification on 5, 10, 15, 20 vectors as a basis
+
+def read_set_and_labels(file_set, file_label):
+    set_list = []
+    with open(file_set) as f:
+        for line in f:
+            set_list.append(line)
